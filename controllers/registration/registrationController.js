f1bb7feff2f56e6f23291f6276e214fe9e8d240c
Krishna Das <krishnadas2806@gmail.com>
2026-02-26 01:37:38 +0530

    Team event members backend with Unstop Features

--- controllers/registration/registrationController.js
+++ controllers/registration/registrationController.js
@@ -6,6 +6,18 @@ const {
   markAttendance,
   exportAttendance,
 } = require("./markAttendance");
+const { leaveTeam } = require("./leaveTeam");
+const { inviteTeamMember } = require("./inviteTeamMember");
+const { getTeamInviteLink } = require("./getTeamInviteLink");
+const { renameTeam } = require("./renameTeam");
+// [v2] New team management controllers
+const { createTeam } = require("./createTeam");
+const { searchTeams } = require("./searchTeams");
+const { joinTeam } = require("./joinTeam");
+const { sendJoinRequest } = require("./sendJoinRequest");
+const { respondJoinRequest } = require("./respondJoinRequest");
+const { checkJoinRequestUpdates } = require("./checkJoinRequestUpdates");
+const { checkAllJoinRequestUpdates } = require("./checkAllJoinRequestUpdates");
 
 module.exports = {
   addRegistration,
@@ -14,4 +26,16 @@ module.exports = {
   getAttendanceCode,
   markAttendance,
   exportAttendance,
+  leaveTeam,
+  inviteTeamMember,
+  getTeamInviteLink,
+  renameTeam,
+  // [v2] New team management exports
+  createTeam,
+  searchTeams,
+  joinTeam,
+  sendJoinRequest,
+  respondJoinRequest,
+  checkJoinRequestUpdates,
+  checkAllJoinRequestUpdates,
 };
